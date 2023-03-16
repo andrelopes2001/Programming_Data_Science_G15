88001b04171231e0f70be93037ec852be4bf2854
andrelopes2001 <119293245+andrelopes2001@users.noreply.github.com>
2023-03-16 00:24:36 +0000

    updated demo file

--- Proji/help functions.sql
+++ Proji/help functions.sql
@@ -13,7 +13,35 @@
 -- where id NOT IN (
 -- select id from db_weather.station);
 
-select min(inv.first_year) from db_weather.inventory inv where inv.element = 'PRCP';
+-- select min(inv.first_year) from db_weather.inventory inv where inv.element = 'PRCP';
+
+-- 5.V
+SELECT 
+    country_name,
+    MONTH(time_id) AS month,
+    AVG(temp_range) AS highest_avg_temp_range
+FROM
+    (SELECT 
+        time_id,
+            ds.country_name,
+            MAX(value) - MIN(value) AS temp_range
+    FROM
+        factobservation fo
+    JOIN dimstation ds ON fo.station_id = ds.id
+    GROUP BY time_id , ds.country_name
+    ORDER BY ds.country_name) t1
+GROUP BY MONTH(time_id) , country_name
+ORDER BY highest_avg_temp_range DESC;
+
+
+select value, country_name from factobservation fo
+join dimstation ds on fo.station_id = ds.id
+where country_name = 'Australia';
+
+select element_id, value, country_name from factobservation fo
+join dimstation ds on fo.station_id = ds.id
+where country_name = 'Australia'
+order by country_name;
 
 
 
